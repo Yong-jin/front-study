d3f058fa06905ba98a431fe5df070616357947e8
Yong-jin <takeardor@gmail.com>
2024-11-18 16:49:50 +0900

    자바 스크립트 연산자/객체,배열/함수

---
+++ javascript/obj02.js
@@ -0,0 +1,28 @@
+//obj02.js
+
+let scores = [10, 30, 50, 40];
+
+//for 반복문
+
+//for
+for(let i=0; i<scores.length; i++){
+    console.log(scores[i]);
+}
+
+//for in
+for(let i in scores){
+    console.log(i + ' ' + scores[i]);
+}
+
+//for of
+//item value score
+for(let item of scores){  //for(String item : stringArr)
+    console.log(item);
+}
+
+//forEach
+//[10, 30, 50, 40]
+                //  (item, index)=> { }
+scores.forEach( (item, index)=>{
+    console.log(item + ' ' + index);
+});
